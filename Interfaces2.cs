8597ef691a55ba1ab63c61a264d2fbf5e4175688
RÜVEYDA KEMİK <77358367+donduruveydakemiik@users.noreply.github.com>
2023-02-07 17:13:38 +0300

    Add files via upload
    Interface kullanımı örneği2

---
+++ Interfaces2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İnterfaces
+{
+    internal interface IWork
+    {
+        void work();        
+    }
+
+    internal interface IEat
+    {
+        void eat();
+    }
+
+    internal interface IgetSalary
+
+    {
+        void getSalary();
+    }
+
+    class robot : IWork
+    {
+        public void work()
+        {
+            Console.WriteLine("Robot çalıştı.");
+        }
+    }
+
+    class manager : IWork, IEat, IgetSalary
+
+    {
+        public void eat()
+        {
+            Console.WriteLine("Yönetici yemek yedi");
+            
+        }
+
+        public void getSalary()
+        {
+
+            Console.WriteLine("Yönetici maaş aldı.");
+        }
+
+        public void work()
+        {
+            Console.WriteLine("Yönetici çalıştı.");
+        }
+    }
+
+    class worker : IWork, IEat, IgetSalary
+
+    {
+        public void eat()
+        {
+            Console.WriteLine("Çalışan yemek yedi");
+        }
+
+        public void getSalary()
+        {
+            Console.WriteLine("Çalışan maaş aldı.");
+        }
+            public void work()
+        {
+            Console.WriteLine("Çalışan çalıştı.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İnterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWork[] work = new IWork[] { new manager(), new robot(), new worker() };
+            foreach (var worker in work)
+            {
+                worker.work();
+            }
+
+            Console.WriteLine("---------------------------------------------------");
+
+            IEat[] eat = new IEat[] { new manager(), new worker() };
+            foreach (var worker in eat)
+            {
+                worker.eat();
+            }
+
+            Console.WriteLine("---------------------------------------------------");
+
+            IgetSalary[] salary = new IgetSalary[] { new worker(), new manager() };
+            foreach (var worker in salary)
+            {
+                worker.getSalary();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
